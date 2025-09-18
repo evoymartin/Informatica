54d1718d4861c45c1b2503561e9cabb28d54e6ba
evoymartin <amadeoevocancino@gmail.com>
2025-09-18 15:11:24 -0300

    Add files via upload

---
+++ Ejercicio1.js
@@ -0,0 +1,8 @@
+let btn= document.
+getElementById("botón");
+let párrafo= document.
+getElementById("texto");
+btn.addEventListener(click,
+    function(){
+        párrafo.innerHTML="Martín"
+    });
